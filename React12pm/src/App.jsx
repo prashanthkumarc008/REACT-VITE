86b5c88e16267f64aeac17bc50e86acb3d67ae40
prashanthkumarc008 <prashanthkumarc008@gmail.com>
2025-04-27 12:27:56 +0530

    updated

--- React12pm/src/App.jsx
+++ React12pm/src/App.jsx
@@ -5,6 +5,7 @@ function App(){
     return(
         <>
             <div>
+            
                 <h1>hgefyuegd</h1>
                 <h1>hgffjjhgjhbngfgjg</h1>
                 <p>hgdvcjhdvcjh</p>
--- React12pm/src/index.jsx
+++ React12pm/src/index.jsx
@@ -1,6 +1,10 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import {createRoot} from 'react-dom/client'
+import BasicExample from './component/BasicExample.jsx'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(<> 
+                                                    <BasicExample/>
                                                     <App/>
-                                                    )
+
+                                                    </>)
