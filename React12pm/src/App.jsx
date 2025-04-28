96bbb889cb394dc2522581fe1e2c2f86770c31ae
prashanthkumarc008 <prashanthkumarc008@gmail.com>
2025-04-28 13:09:44 +0530

    updated

--- React12pm/src/App.jsx
+++ React12pm/src/App.jsx
@@ -1,6 +1,7 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import BasicExample from './component/BasicExample';
 import Login from './component/login';
+import Message from './component/Message';
 
 
 function App(){
@@ -11,6 +12,7 @@ function App(){
             <div>
             <BasicExample/>
             <Login/>
+            <Message/>
             </div>
         </>
     )
---
+++ React12pm/src/component/Message.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Container } from "react-bootstrap";
+class Message extends Component{
+    render(){
+        return <div>
+        <Container>
+            <h1>This is class component</h1>
+                <h2>Good Morning</h2>
+        </Container>
+            
+        
+        </div>
+    }
+}
+
+export default Message;
