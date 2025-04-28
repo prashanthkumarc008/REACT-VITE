a434a8b17167d0ea71dc40aa782790ad5607a150
prashanthkumarc008 <prashanthkumarc008@gmail.com>
2025-04-28 17:28:22 +0530

    updated

--- React12pm/src/component/MovieName.jsx
+++ React12pm/src/component/MovieName.jsx
@@ -1,9 +1,8 @@
 
 function MovieName(props){
-    return(
-    <div>
-        <h1> Movie name is : {props.brand}</h1>
-    </div>
-    )
+    return ( <div>
+                 <h1> Movie name is : {props.brand}</h1>
+            </div>)
+    
 }
 export default MovieName;
