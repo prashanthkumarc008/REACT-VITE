6abea631393d59a608795e2528e1ba8ac8aa212c
prashanthkumarc008 <prashanthkumarc008@gmail.com>
2025-05-02 13:00:51 +0530

    updated

--- React12pm/src/component/gmgfgn.jsx
+++ React12pm/src/component/gmgfgn.jsx
@@ -10,9 +10,9 @@ let Gmgfgn=()=>{
             <Container>
                   <h1> Gm/Gf/Gn Component</h1>
             <h1> hello : {msg}</h1>
-            <button onClick={()=>{setMsg("gm")}}>GM</button>
-            <button onClick={()=>{setMsg("ga")}}>ga</button>
-            <button onClick={()=>{setMsg("gn")}}>gn</button>
+            <button onClick={()=>{setMsg("good morning")}}>GM</button>
+            <button onClick={()=>{setMsg("good afternoon")}}>ga</button>
+            <button onClick={()=>{setMsg("good night")}}>gn</button>
 
             </Container>
           
--- React12pm/src/propsEx/User.jsx
+++ React12pm/src/propsEx/User.jsx
@@ -6,13 +6,13 @@ let User=()=>{
         uname:"ananda",
         loc:["ind","bng","marathahalli"],
         email:'ananda@gmail.com'
-
     }
+    let array=[100,101,102,103]
     return(
         <div>
         <Container> 
             <h1> User Component</h1>
-            <Emp one={"gm"} emp={user_data}/>
+            <Emp one={"gm"} emp={user_data} arr={array}/>
         </Container>
        
        
