c79a6be15a7a6955b5ec652125fe581942f5fcc1
prashanthkumarc008 <prashanthkumarc008@gmail.com>
2025-05-09 23:03:41 +0530

    updated

--- React12pm/src/form/FormUser.jsx
+++ React12pm/src/form/FormUser.jsx
@@ -3,43 +3,41 @@ import { useState } from "react";
 function FormUser() {
   const [user, setUser] = useState({ email: "", password: "" });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setUser((prevUser) => ({
-      ...prevUser,
-      [name]: value
-    }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("User:", user);
-    alert(JSON.stringify(user));
+  const handleForm = (e) => {
+    if (e.type === "change") {
+      const { name, value } = e.target;
+      setUser((prevUser) => ({
+        ...prevUser,
+        [name]: value
+      }));
+    } else if (e.type === "submit") {
+      e.preventDefault();
+      console.log("User:", user);
+      alert(JSON.stringify(user));
+    }
   };
 
   return (
     <div className="container">
-        <pre>{JSON.stringify(user)}</pre>
-        <form onSubmit={handleSubmit}>
-      <input
-        type="email"
-        name="email"
-        value={user.email}
-        onChange={handleChange}
-        placeholder="Email"
-      /><br/>
-      <input
-        type="password"
-        name="password"
-        value={user.password}
-        onChange={handleChange}
-        placeholder="Password"
-      /><br/>
-      <button type="submit">Submit</button>
-    </form>
+      <pre>{JSON.stringify(user)}</pre>
+      <form onSubmit={handleForm}>
+        <input
+          type="email"
+          name="email"
+          value={user.email}
+          onChange={handleForm}
+          placeholder="Email"
+        /><br/>
+        <input
+          type="password"
+          name="password"
+          value={user.password}
+          onChange={handleForm}
+          placeholder="Password"
+        /><br/>
+        <button type="submit">Submit</button>
+      </form>
     </div>
-        
-    
   );
 }
 
