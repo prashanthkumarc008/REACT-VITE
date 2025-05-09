f26142d43523b4e2298985415c8f5ab4fafa7c83
prashanthkumarc008 <prashanthkumarc008@gmail.com>
2025-05-09 13:18:04 +0530

    update

--- React12pm/src/form/FormUser.jsx
+++ React12pm/src/form/FormUser.jsx
@@ -1,16 +1,29 @@
-import { useState } from "react"
+import { useState } from "react";
 
-let FormUser=()=>{
-    let [username,setUsername]=useState("Enter UserName")
-
-    return(
-        <div>
-            <form>
-                <label> username : <input type="text"></input></label>
-                <label> password : <input type="text"></input></label>
-
+let Login = ()=>{
+    let [user,setUser]  = useState({"email":"","password":""})
+    let emailHandler = (event)=>{
+        console.log(event)
+        console.log(event.target)
+        console.log(event.target.value)
+        setUser({...user,email:event.target.value})
+    }
+    let pwdHandler=(event)=>{
+        setUser({...user,password:event.target.value})
+    }
+    let submitHandler = ()=>{
+        //send data to backend or api
+        console.log(user)
+        alert(JSON.stringify(user))
+    }
+    return <div>
+            <pre>{JSON.stringify(user)}</pre>
+            <h4>Login Details</h4>
+            <form onSubmit={submitHandler} >
+            EmailId::::: <input type="text" onInput={emailHandler} />  <br /> <br />
+            Password: <input type="password" onInput={pwdHandler} /> <br /> <br />
+            <input type="submit" value={"Login"} />
             </form>
-        </div>
-    )
+            </div>
 }
-export default FormUser;
+export default Login;
