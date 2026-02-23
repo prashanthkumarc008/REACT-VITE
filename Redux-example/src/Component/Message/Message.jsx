df60849ee5a9a3d775266e6a66e5718c7cb43cd2
Prashanth Kumar C <prashanthkumarc008@gmail.com>
2026-02-23 19:55:58 +0530

    Update Message.jsx

--- Redux-example/src/Component/Message/Message.jsx
+++ Redux-example/src/Component/Message/Message.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { increment, decrement } from '../actions/counterActions';
 
+
+
 const Message = () => {
   // Access state from Redux store
   const count = useSelector((state) => state.count);
